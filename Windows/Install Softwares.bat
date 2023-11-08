d63e87cb6bf923586851ceaad58dd7c84a241ffc
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2023-11-08 04:58:09 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -1,12 +1,8 @@
 winget install WhatsApp -s msstore --accept-package-agreements
-winget install Terminal -s msstore --accept-package-agreements
-winget install -e --id SumatraPDF.SumatraPDF
 winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id RevoUninstaller.RevoUninstallerPro
-winget install -e --id Cryptomator.Cryptomator
+winget install -e --id Webroot.SecureAnywhere
 winget install -e --id BleachBit.BleachBit
 winget install -e --id sylikc.JPEGView
-winget install -e --id Peppy.Osu!
 winget install -e --id Nvidia.GeForceNow
 winget install -e --id Zoom.Zoom
 winget install -e --id Google.Drive
@@ -14,9 +10,5 @@ winget install -e --id Valve.Steam
 winget install -e --id Discord.Discord
 winget install -e --id Anki.Anki
 winget install -e --id Transmission.Transmission
-winget install -e --id 7zip.7zip
 winget install -e --id Mozilla.Firefox
-winget install -e --id Henry++.simplewall
-winget install -e --id AdGuard.AdGuard
-winget install -e --id voidtools.Everything
-winget install -e --id Stardock.Start11
+winget install -e --id zhanglun.Lettura
