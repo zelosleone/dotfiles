ab2e8e0764fd39c8f6124ae08f2c28a1b5eedccc
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2023-11-25 06:31:41 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -1,33 +1,23 @@
 winget install WhatsApp -s msstore --accept-package-agreements
+winget install Terminal -s msstore --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Webroot.SecureAnywhere
-winget install -e --id BleachBit.BleachBit
-winget install -e --id sylikc.JPEGView
+winget install -e --id nomacs.nomacs
 winget install -e --id Nvidia.GeForceNow
 winget install -e --id Zoom.Zoom
 winget install -e --id Google.Drive
 winget install -e --id Valve.Steam
 winget install -e --id Discord.Discord
-winget install -e --id zhanglun.Lettura
 winget install -e --id Surfshark.Surfshark
 winget install -e --id Telegram.TelegramDesktop
 winget install -e --id Spotify.Spotify
 winget install -e --id Stellarium.Stellarium
-winget install -e --id Squitch.Tess
-winget install -e --id SomePythonThings.WingetUIStore
-winget install -e --id OpenWhisperSystems.Signal
-winget install -e --id Ombrelin.PandocGui
-winget install -e --id MinhLoi.Midterm
 winget install -e --id MacPaw.Encrypto
 winget install -e --id LedgerHQ.LedgerLive
 winget install -e --id Klocman.BulkCrapUninstaller
 winget install -e --id KDE.Okular
-winget install -e --id IObit.DriverBooster
-winget install -e --id DataHammer.HammerPDF
-winget install -e --id Brave.Brave
-winget install -e --id BiniSoft.WindowsFirewallControl
+winget install -e --id Google.Chrome
 winget install -e --id agalwood.Motrix
 winget install -e --id Audacity.Audacity
 winget install -e --id Notion.Notion
-winget install -e --id GeoGebra.Classic.5
 winget install -e --id GeoffreyChen.Paperlib
