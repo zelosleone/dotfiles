d4bc380babf16674e22e0376644e9f5b5f23fa95
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2023-12-12 21:28:15 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -1,23 +1,17 @@
 winget install WhatsApp -s msstore --accept-package-agreements
-winget install Terminal -s msstore --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Webroot.SecureAnywhere
-winget install -e --id nomacs.nomacs
 winget install -e --id Nvidia.GeForceNow
 winget install -e --id Zoom.Zoom
 winget install -e --id Google.Drive
 winget install -e --id Valve.Steam
 winget install -e --id Discord.Discord
-winget install -e --id Surfshark.Surfshark
 winget install -e --id Telegram.TelegramDesktop
 winget install -e --id Spotify.Spotify
 winget install -e --id Stellarium.Stellarium
-winget install -e --id MacPaw.Encrypto
 winget install -e --id Klocman.BulkCrapUninstaller
 winget install -e --id KDE.Okular
 winget install -e --id Google.Chrome
 winget install -e --id agalwood.Motrix
 winget install -e --id Audacity.Audacity
 winget install -e --id Notion.Notion
-winget install -e --id GeoffreyChen.Paperlib
-winget install --id=RazerInc.RazerInstaller  -e
+winget install -e --id RemNote.RemNote
