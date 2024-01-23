c62856fd4a88628ca7059251099e2097792153fd
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2024-01-23 15:24:49 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -2,16 +2,9 @@ winget install WhatsApp -s msstore --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Nvidia.GeForceNow
 winget install -e --id Zoom.Zoom
-winget install -e --id Google.Drive
 winget install -e --id Valve.Steam
-winget install -e --id Discord.Discord
-winget install -e --id Telegram.TelegramDesktop
 winget install -e --id Spotify.Spotify
 winget install -e --id Stellarium.Stellarium
 winget install -e --id Klocman.BulkCrapUninstaller
-winget install -e --id KDE.Okular
-winget install -e --id Google.Chrome
 winget install -e --id agalwood.Motrix
 winget install -e --id Audacity.Audacity
-winget install -e --id Notion.Notion
-winget install -e --id RemNote.RemNote
