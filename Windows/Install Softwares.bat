48df59b23fc03d250ff28e00695e477d74aa44f2
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2024-12-06 03:34:43 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -1,4 +1,24 @@
 winget install WhatsApp -s msstore --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
-winget install -e --id Spotify.Spotify
+winget install -e --id Anaconda.Anaconda3
+winget install -e --id Git.Git
+winget install -e --id Docker.DockerDesktop
+winget install -e --id Microsoft.Office
+winget install -e --id Transmission.Transmission
+winget install -e --id=Microsoft.DotNet.Framework.DeveloperPack_4
+winget install -e --id=Julialang.Juliaup
+winget install -e --id=OpenJS.NodeJS
+winget install -e --id=Zoom.Zoom
+winget install -e --id=Telegram.TelegramDesktop
+winget install -e --id=Discord.Discord
+winget install -e --id=Google.GoogleDrive
+winget install -e --id=Anki.Anki
+winget install -e --id=Microsoft.PowerShell
+winget install -e --id=voidtools.Everything
+winget install -e --id=Nvidia.GeForceNow
+winget install -e --id=KDE.Okular
+winget install -e --id=GeoGebra.Classic
+winget install -e --id=Posit.RStudio
+winget install -e --id=RProject.R
+winget install -e --id=Tableau.Public
