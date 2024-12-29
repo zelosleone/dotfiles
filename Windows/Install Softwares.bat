34b06005e6cbf2d1ca21841d0b6c29a24318bdf1
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2024-12-29 03:43:12 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -2,13 +2,10 @@ winget install WhatsApp -s msstore --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
 winget install -e --id Anaconda.Anaconda3
-winget install -e --id Git.Git
 winget install -e --id Docker.DockerDesktop
 winget install -e --id Microsoft.Office
 winget install -e --id Transmission.Transmission
-winget install -e --id=Microsoft.DotNet.Framework.DeveloperPack_4
-winget install -e --id=Julialang.Juliaup
-winget install -e --id OpenJS.NodeJS.LTS
+winget install -e --id Julialang.Julia
 winget install -e --id=Zoom.Zoom
 winget install -e --id=Telegram.TelegramDesktop
 winget install -e --id=Discord.Discord
@@ -16,25 +13,22 @@ winget install -e --id=Google.GoogleDrive
 winget install -e --id=Anki.Anki
 winget install -e --id=Microsoft.PowerShell
 winget install -e --id=voidtools.Everything
-winget install -e --id=Nvidia.GeForceNow
 winget install -e --id=KDE.Okular
 winget install -e --id=GeoGebra.Classic
-winget install -e --id=Posit.RStudio
-winget install -e --id=RProject.R
 winget install -e --id=Tableau.Public
 winget install -e --id=junegunn.fzf
 winget install -e --id=DupeGuru.DupeGuru
 winget install -e --id=Microsoft.PowerToys
-winget install -e --id=7zip.7zip
-winget install -e --id=RandyRants.SharpKeys
 winget install -e --id=Nvidia.CUDA
-winget install -e --id=JetBrains.DataSpell
 winget install -e --id=stnkl.EverythingToolbar.Beta
+winget install -e --id=JetBrains.DataSpell
 winget install -e --id=JetBrains.DataGrip
 winget install -e --id=JetBrains.dotUltimate
 winget install -e --id=JetBrains.ReSharper
 winget install -e --id=JetBrains.PyCharm.Professional
 winget install -e --id=JetBrains.CLion
 winget install -e --id=JetBrains.WebStorm
-winget install -e --id=JetBrains.RubyMine
 winget install -e --id=JetBrains.GoLand
+winget install -e --id=RedHat.Podman
+winget install -e --id=Anysphere.Cursor
+winget install -e --id=GitHub.GitHubDesktop
