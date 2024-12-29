e4286c5ad680462812500a60824d3fb5f9d25594
Denizhan Dakılır <71423969+zelosleone@users.noreply.github.com>
2024-12-29 20:58:43 +0300

    Update Install Softwares.bat

--- Windows/Install Softwares.bat
+++ Windows/Install Softwares.bat
@@ -1,14 +1,14 @@
 winget install WhatsApp -s msstore --accept-package-agreements
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
-winget install -e --id Anaconda.Anaconda3
 winget install -e --id Docker.DockerDesktop
+winget install -e --id=RedHat.Podman
 winget install -e --id Microsoft.Office
 winget install -e --id Transmission.Transmission
-winget install -e --id Julialang.Julia
 winget install -e --id=Zoom.Zoom
 winget install -e --id=Telegram.TelegramDesktop
 winget install -e --id=Discord.Discord
+winget install -e --id=Element.Element
 winget install -e --id=Google.GoogleDrive
 winget install -e --id=Anki.Anki
 winget install -e --id=Microsoft.PowerShell
@@ -16,13 +16,12 @@ winget install -e --id=voidtools.Everything
 winget install -e --id=KDE.Okular
 winget install -e --id=GeoGebra.Classic
 winget install -e --id=Tableau.Public
+winget install -e --id=Obsidian.Obsidian
 winget install -e --id=junegunn.fzf
 winget install -e --id=DupeGuru.DupeGuru
-winget install -e --id=Microsoft.PowerToys
 winget install -e --id=Nvidia.CUDA
 winget install -e --id=stnkl.EverythingToolbar.Beta
 winget install -e --id=JetBrains.Toolbox
-winget install -e --id=RedHat.Podman
 winget install -e --id=Anysphere.Cursor
 winget install -e --id=GitHub.GitHubDesktop
 winget install -e --id=GitHub.cli
